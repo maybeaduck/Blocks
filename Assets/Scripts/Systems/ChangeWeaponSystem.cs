13d931f55673ccca922593989723f2e8cbf386b1
s.korolev <korolev.s@cedargame.com>
2021-09-17 00:28:15 +0300

    add changes weapon system

--- Assets/Scripts/Systems/ChangeWeaponSystem.cs
+++ Assets/Scripts/Systems/ChangeWeaponSystem.cs
@@ -22,11 +22,17 @@ namespace Zlodey
 
             Debug.Log($"scroll: {scroll}");
             Debug.Log($"IndexWeaponToList: {_runtimeData.IndexWeaponToList}");
+            Debug.Log($"AvailableWeapons.Count: {_runtimeData.AvailableWeapons.Count}");
 
-            if (_runtimeData.IndexWeaponToList < 0) _runtimeData.IndexWeaponToList = _runtimeData.AvailableWeapons.Count;
+            if (_runtimeData.IndexWeaponToList < 0) _runtimeData.IndexWeaponToList = _runtimeData.AvailableWeapons.Count - 1;
             if (_runtimeData.IndexWeaponToList >= _runtimeData.AvailableWeapons.Count) _runtimeData.IndexWeaponToList = 0;
 
             _runtimeData.CurrentWeapon = _runtimeData.AvailableWeapons[_runtimeData.IndexWeaponToList];
+
+            for (int i = 0; i < _runtimeData.AvailableWeapons.Count; i++)
+            {
+                _runtimeData.AvailableWeapons[i].gameObject.SetActive(i == _runtimeData.IndexWeaponToList);
+            }
         }
     }
 }
