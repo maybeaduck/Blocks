0119ed9a9dfb919e48097670202936c02413f86b
s.korolev <korolev.s@cedargame.com>
2021-09-16 23:28:19 +0300

    fix animation axe

--- Assets/Scripts/Systems/WeaponAttackSystem.cs
+++ Assets/Scripts/Systems/WeaponAttackSystem.cs
@@ -6,9 +6,9 @@ namespace Zlodey
     {
         public void Run()
         {
-            if (_runtimeData.IsAttack && _runtimeData.WeaponView)
+            if (_runtimeData.WeaponView)
             {
-                _runtimeData.WeaponView.Animator.SetTrigger("Attack");
+                _runtimeData.WeaponView.Animator.SetBool("IsAttack", _runtimeData.IsAttack);
             }
         }
     }
