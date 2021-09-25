0fbdff61c9e724871c375d38cff88d7bb1b4ef1c
maybeadeer <maybeadeer@gmail.com>
2021-09-25 03:28:51 +0300

    new block and tools

---
+++ Assets/Scripts/MoveServiceSystem.cs
@@ -0,0 +1,13 @@
+using Leopotam.Ecs;
+
+namespace Zlodey
+{
+    internal class MoveServiceSystem : IEcsRunSystem
+    {
+        
+        public void Run()
+        {
+            
+        }
+    }
+}
